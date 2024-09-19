d9ef066405dbcf2e9b1b763109a32555f4a2602e
fchristian1 <fchristian1@gmx.net>
2024-09-19 21:46:28 +0200

    20240919-214628

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <></>;
+    return (
+        <>
+            <h1>Empty React Project</h1>
+        </>
+    );
 }
 
 export default App;
