b0a8f26498ad0f94f7c28d22d0d3fbc2115c07bd
fchristian1 <fchristian1@gmx.net>
2024-09-22 11:29:15 +0200

    20240922-112915

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
 function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <>
-            <h1>Empty React Project</h1>
+            <h1 className="text-4xl text-orange-500 underline">
+                viteReactEmpty Project
+            </h1>
         </>
     );
 }
